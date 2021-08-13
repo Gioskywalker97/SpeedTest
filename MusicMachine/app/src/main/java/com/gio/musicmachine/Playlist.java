9adb0bbb71b1cacd1717467e4585de92d9af6db6
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2021-08-13 09:38:00 -0400

    Delete MusicMachine directory

--- MusicMachine/app/src/main/java/com/gio/musicmachine/Playlist.java
+++
@@ -1,9 +0,0 @@
-package com.gio.musicmachine;
-
-public class Playlist {
-    public static String[] songs = {"He’s the Greatest Dancer",
-            "Cut the Cake",
-            "The Groove Line",
-            "Stayin’ Alive",
-            "Give Up the Funk"};
-}
