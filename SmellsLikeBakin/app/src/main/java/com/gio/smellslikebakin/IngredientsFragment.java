3c5992387e72b764949cef2c6a2a33012c45333b
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2021-08-13 09:38:14 -0400

    Delete SmellsLikeBakin directory

--- SmellsLikeBakin/app/src/main/java/com/gio/smellslikebakin/IngredientsFragment.java
+++
@@ -1,8 +0,0 @@
-package com.gio.smellslikebakin;
-
-public class IngredientsFragment extends CheckBoxesFragment{
-    @Override
-    public String[] getContents(int index) {
-        return Recipes.ingredients[index].split("`");
-    }
-}
