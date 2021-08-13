9951b42c576d5abd3c01d5e81afe0adb2dbf3c8e
Giovanni Salas <84675680+Gioskywalker97@users.noreply.github.com>
2021-08-13 09:38:40 -0400

    Delete TestingProject directory

--- TestingProject/app/src/main/java/com/teamtreehouse/testingbase/MainActivityView.java
+++
@@ -1,7 +0,0 @@
-package com.teamtreehouse.testingbase;
-
-public interface MainActivityView {
-    void changeTextViewText(String text);
-    void changeBackgroundColor(int color);
-    void launchOtherActivity(Class activity);
-}
